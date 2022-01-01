2bbc1ba20ee89250c7556967f0ac40d7087d1258
Usama <khawajausama775@gmail.com>
2022-01-01 19:27:31 +0500

    Added bottom tabs and safe area view

--- App.tsx
+++ App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import {SafeAreaView, StatusBar, View} from 'react-native';
 import Signin from './screens/Signin';
 import SplashScreen from './screens/Splash';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Signup from './screens/Signup';
-import Wallet from './screens/wallet';
+import Tabs from './screens/Tabs';
+import {Colors} from './constants';
 
 const App = () => {
   const Stack = createNativeStackNavigator();
 
   return (
     <>
-      <StatusBar backgroundColor="#EF8E35" />
+      <View style={{backgroundColor: Colors.base}}>
+        <SafeAreaView>
+          <StatusBar backgroundColor={Colors.base} />
+        </SafeAreaView>
+      </View>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
           <Stack.Screen name="Splash" component={SplashScreen} />
           <Stack.Screen name="Signin" component={Signin} />
           <Stack.Screen name="Signup" component={Signup} />
-          <Stack.Screen name="Wallet" component={Wallet} />
+          <Stack.Screen name="Wallet" component={Tabs} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ screens/Tabs/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import Wallet from '../wallet';
+import {Image} from 'react-native';
+//@ts-ignore
+import WalletIcon from '../../assets/Icons/wallet-transparent.png';
+//@ts-ignore
+import HistoryIcon from '../../assets/Icons/history.png';
+import {Colors} from '../../constants';
+import Drawer from '../Drawer';
+
+const Tab = createMaterialBottomTabNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator
+      barStyle={{backgroundColor: Colors.base}}
+      initialRouteName="Wallet"
+      activeColor="white"
+      shifting
+      >
+      <Tab.Screen
+        name="Wallet"
+        component={Wallet}
+        options={{
+          tabBarIcon: () => (
+            <Image source={WalletIcon} style={{height: 25, width: 25}} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="History"
+        component={Wallet}
+        options={{
+          tabBarIcon: () => (
+            <Image source={HistoryIcon} style={{height: 25, width: 25}} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default Tabs;
